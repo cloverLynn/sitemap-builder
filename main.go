18bb553e30495792848665bab40427b19c3d92e3
cForbes <clover.forbes96@gmail.com>
2024-04-13 00:25:42 -0400

    layed out skeleton

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// TODO SiteMap Struct (MAYBE NOT NEEDED)
+type SiteMap struct {
+}
+
+// TODO getPage
+func getHTML(u string) {
+
+}
+
+// TODO output XML
+func outXML() {
+
+}
+
+// TODO lgoic
+func getLinks() {
+
+}
+
+func main() {
+	fmt.Println("TODO EVERYTHING")
+}
